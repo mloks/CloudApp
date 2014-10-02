0c487e6ee38c61ebdc3e769b5d91fe55a90c1ff7
Sasha M. <meloks@yahoo.com>
2014-10-02 17:43:35 +0100

    Contact templates changes.

--- app/client/src/modules/Common/controllers/dashboard.js
+++ app/client/src/modules/Common/controllers/dashboard.js
@@ -3,7 +3,7 @@ var DashboardController = function($scope, $http) {
   $scope.result = {};
   $scope.isUpdating = true;
 
-  $http({url: '/api/crm/list', responseType: 'json', params: {}}).then(
+  $http({url: '/api/entity/contact', responseType: 'json', params: {}}).then(
     function (response) {
       $scope.result = response.data;
       $scope.isUpdating = false;
